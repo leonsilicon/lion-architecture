ed436fec716c468c9a071e60dca5a4799c8f1cef
Leon Si <contact@leonzalion.com>
2021-12-01 12:10:16 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-export * from './create';
-export {
-	TypescriptTemplateInstance,
-	TypescriptTemplateInstanceOptions,
-} from './types/instance';
+import rfdc from 'rfdc';
+
+export function retrieveModuleProperties<P>(modules: Record<string, Function>) {
+	const modulesObj = { ...modules };
+	const moduleProperties = {} as P;
+	for (const module of Object.values(modulesObj)) {
+		for (const [fn, value] of Object.entries(module())) {
+			moduleProperties[fn as keyof P] = value as any;
+		}
+	}
+	return moduleProperties;
+}
+
+const clone = rfdc();
+export function createInstance(
+	properties: Record<string, unknown>,
+	state: Record<string, unknown>
+): unknown {
+	return Object.assign(clone(properties), state);
+}
--- src/utils/methods.ts
+++
@@ -1,9 +0,0 @@
-import type { InternalTypescriptTemplateInstance } from '../types/instance';
-
-export function createMethodDefiner<T>() {
-	return function defineMethods<M>(
-		methods: M & ThisType<InternalTypescriptTemplateInstance<T>>
-	) {
-		return methods as unknown as M;
-	};
-}
